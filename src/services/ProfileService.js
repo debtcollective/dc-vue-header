d4ebb026eeb0864c20c2604ca142384339232ed7
Alexis Duran <alexis.duranm@gmail.com>
2018-11-22 16:41:26 -0400

    Static update to ProfileService and minor service update

--- src/services/ProfileService.js
+++ src/services/ProfileService.js
@@ -3,23 +3,28 @@ import {
   deleteOptions,
   discourseEndpoint,
   toolsEndpoint,
-  currentUsername,
-} from './service';
+  currentUsername
+} from "./service";
 
-export const getProfileLink = () => `${discourseEndpoint()}/u/${currentUsername()}`;
+export const getProfileLink = () =>
+  `${discourseEndpoint()}/u/${currentUsername()}`;
 
 export const getInboxLink = () => `${getProfileLink()}/messages`;
 
 export const getCurrentUser = () =>
-  fetch(`${discourseEndpoint()}/about/live_post_counts.json`, getOptions()).then(res => {
-    if (res.status === 403) {
-      throw res; // probably just no one was logged in
+  fetch(
+    `${discourseEndpoint()}/about/live_post_counts.json`,
+    getOptions()
+  ).then(res => {
+    if (res.status === 403 || res.error_type === "not_logged_in") {
+      // probably just no one was logged in, besides, it will show a console error https://goo.gl/FgfJcr
+      return { user: null };
     } else {
-      const username = res.headers.get('X-Discourse-Username');
+      const username = res.headers.get("X-Discourse-Username");
       return fetch(`${discourseEndpoint()}/u/${username}`, getOptions())
         .then(r => r.json())
         .then(user => {
-          return (window['@@current-user'] = user);
+          return (window["@@current-user"] = user);
         });
     }
   });
@@ -28,14 +33,17 @@ export const getCsrfToken = () =>
   fetch(`${discourseEndpoint()}/session/csrf.json`, getOptions())
     .then(res => res.json())
     .then(({ csrf }) => {
-      window['@@csrf-token'] = csrf;
+      window["@@csrf-token"] = csrf;
     });
 
-const toBase = () => (window.location = '/');
+const toBase = () => (window.location = "/");
 
 export const logout = () =>
-  fetch(`${discourseEndpoint()}/session/${currentUsername()}`, deleteOptions()).catch(() =>
-    fetch(`${toolsEndpoint()}/logout`, { credentials: 'include' })
+  fetch(
+    `${discourseEndpoint()}/session/${currentUsername()}`,
+    deleteOptions()
+  ).catch(() =>
+    fetch(`${toolsEndpoint()}/logout`, { credentials: "include" })
       .then(toBase)
-      .catch(toBase),
+      .catch(toBase)
   );
